2e557b652adbe4e3573c10eefc28150185ff394c
capalvarez <capalvarez.i@gmail.com>
2014-10-14 22:41:34 -0300

    Otro commit para refactoring

--- src/bluej/pkgmgr/AbstractPkgFrame.java
+++ src/bluej/pkgmgr/AbstractPkgFrame.java
@@ -1,6 +1,5 @@
 package bluej.pkgmgr;
 
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,5 +9,14 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
 public class AbstractPkgFrame extends JFrame {
+	 /**
+     * Add a new menu item to a menu.
+     */
+    protected JMenuItem createMenuItem(Action action, JMenu menu)
+    {
+        JMenuItem item = menu.add(action);
+        item.setIcon(null);
+        return item;
+    }
 
 }
